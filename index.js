aaffd1dc11a67f5a38ff862ff4296a317886e194
Viktor Fejes <viktor@viktorfejes.com>
2020-09-28 10:25:52 -0700

    Added package.json

---
+++ index.js
@@ -0,0 +1,40 @@
+const ndarray = require('ndarray')
+
+function toFixedNumber(num, digits, base){
+    var pow = Math.pow(base||10, digits);
+    return Math.round(num*pow) / pow;
+}
+
+function generateHaldData(size) {
+    const cubeSize = Math.sqrt(size)
+    const imageSize = size * cubeSize
+
+    let data = []
+
+    let blue = 0
+    while (blue < cubeSize) {
+        let green = 0
+        while (green < cubeSize) {
+            while (red < cubeSize) {
+                data.push(toFixedNumber(red / (cubeSize - 1), 6))
+                data.push(toFixedNumber(green / (cubeSize - 1), 6))
+                data.push(toFixedNumber(blue / (cubeSize - 1), 6))
+
+                // data.push([
+                //     Math.round((red / (cubeSize - 1)) * 1e6) / 1e6,
+                //     Math.round((green / (cubeSize - 1)) * 1e6) / 1e6,
+                //     Math.round((blue / (cubeSize - 1)) * 1e6) / 1e6,
+                // ])
+                red++
+            }
+            green++
+        }
+        blue++
+    }
+
+    out = ndarray(data, [size, size, size, 3])
+
+    return out
+}
+
+module.exports = generateHaldData
